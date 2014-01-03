83316cbbf4e24c88f1410a6a502db1838b042c1a
Brook Mesfin <tier5@me.com>
2014-01-02 17:02:15 -0800

    Update to do code

--- src/ShapeJPanel.java
+++ src/ShapeJPanel.java
@@ -14,32 +14,45 @@ public class ShapeJPanel extends JPanel {
 	super.paintComponent( g );
 	Random random = new Random();
 	
-	int[] xPoints = {5, 75, 75, 5, -15, -15};
-		//{5, 55, 55, 5, 5, 0, 0, 55, 55, 0, 40 };
-		//{55, 27, 37, 1, 43, 55, 67,109, 73, 83};
-	int[] yPoints = {25, 25, 75, 75, 50, 15}; 
-		//{30, 30, 72, 72, 60, 40, 30, 100, 100, 30, 40 };
-		//{72, 96, 54, 36, 36, 0, 36, 36, 54, 96};
+	int[] xPoints = { 100, 100, 200, 200, 100 };
+	int[] x2Points =  { 75, 75, 175, 175, 75 };
+	
+
+	int[] yPoints = { 100, 200, 200, 100, 100 };
+	int[] y2Points = { 75, 175, 175 ,75, 75 };
 	
 	Graphics2D g2d = ( Graphics2D ) g;
 	GeneralPath cube = new GeneralPath();
 	
 	cube.moveTo( xPoints[0], yPoints[0]);
-	
-	for (int count = 1; count < xPoints.length; count++)
-		cube.lineTo( xPoints [count], yPoints [count] );
-		
+	cube.moveTo( x2Points[0], y2Points[0]);
+
+	for ( int i = 1; i <= 4; i++ ) {
+	// create the first base
+	cube.moveTo( xPoints[ i - 1 ], yPoints[ i - 1 ] );
+	cube.lineTo( xPoints[ i ], yPoints[ i ] );
+
+	// create the second base
+	cube.moveTo( x2Points[ i - 1 ], y2Points[ i - 1 ] );
+	cube.lineTo( x2Points[ i ], y2Points[ i ] );
+
+	// create the lines between the bases
+	cube.moveTo( xPoints[ i ], yPoints[ i ] );
+	cube.lineTo( x2Points[ i ], y2Points[ i ] );
+}
+
 	cube.closePath();
 	
-	g2d.translate( 150, 150 );
-	/*
+	g2d.translate( 400, 400 );
+	
+
 	for (int count = 1; count <= 20; count++) {
 		g2d.rotate(Math.PI / 10.0);
 		
 		g2d.setColor(new Color( random.nextInt( 256 ), 
 				random.nextInt( 256 ), random.nextInt( 256)));
 		g2d.draw( cube );	
-		}*/
+		}
 	g2d.draw( cube );
 	}
 
